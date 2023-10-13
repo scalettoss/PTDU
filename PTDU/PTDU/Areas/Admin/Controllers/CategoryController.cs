dab811fed8232e4eb092f313a778ce92148abe2c
scalettoss <baodeptrai0707@gmail.com>
2023-10-14 00:30:40 +0700

    Create Commit

---
+++ PTDU/PTDU/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.DAO;
+using MyClass.Model;
+using PTDU.Library;
+namespace PTDU.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesDAO categoriesDAO = new CategoriesDAO();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View(categoriesDAO.getList("Index"));
+        }
+
+        // GET: Admin/Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //CREATE
+        // GET: Admin/Category/Create
+        public ActionResult Create()
+        {
+            ViewBag.ListCat = new SelectList(categoriesDAO.getList("Index"), "Id", "Name");
+            ViewBag.ListOrder = new SelectList(categoriesDAO.getList("Index"), "Order", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                //Xu ly tu dong: CreateAt
+                categories.CreateAt = DateTime.Now;
+                //Xu ly tu dong: UpdateAt
+                categories.UpdateAt = DateTime.Now;
+                //Xu ly tu dong: ParentId
+                if (categories.ParentID == null)
+                {
+                    categories.ParentID = 0;
+                }
+                //Xu ly tu dong: Order
+                if (categories.Order == null)
+                {
+                    categories.Order = 1;
+                }
+                else
+                {
+                    categories.Order += 1;
+                }
+                //Xu ly tu dong: Slug
+                categories.Slug = XString.Str_Slug(categories.Name);
+
+                //Chen them dong cho DB
+                categoriesDAO.Insert(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //EDIT
+        // GET: Admin/Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDAO.Update(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        //DELETE
+        // GET: Admin/Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categories categories = categoriesDAO.getRow(id);
+            categoriesDAO.Delete(categories);
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Status(int? id)
+        {
+            if (id == null)
+            {
+                //thong bao that bai
+                TempData["message"] = new XMessage("danger","Cập nhật trạng thái thất bại");
+                return RedirectToAction("Index");
+            }
+            //truy van id
+            Categories categories = categoriesDAO.getRow(id);
+
+            //chuyen doi trang thai cua Satus tu 1<->2
+            categories.Status = (categories.Status == 1) ? 2 : 1;
+
+            //cap nhat gia tri UpdateAt
+            categories.UpdateAt = DateTime.Now;
+
+            //cap nhat lai DB
+            categoriesDAO.Update(categories);
+
+            //thong bao cap nhat trang thai thanh cong
+            TempData["message"] = new XMessage("success", "Cập nhật trạng thái thành công");
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PTDU/PTDU/Controllers/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTDU.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Index()
+        {
+            MyDBContext db = new MyDBContext();//tạo mới mẫu tin
+            int somau = db.Products.Count();//ví dụ hiển thị số mẫu tin của Products ra M.hình
+            ViewBag.somau = somau;//truyền dưới dạng ViewBag
+            return View();
+
+        }
+    }
+}
